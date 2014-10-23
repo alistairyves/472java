d75ea3fd44c865a1aa143ff04b82a92c0fff3955
Alistair Mersereau <alistairyves@gmail.com>
2014-10-23 19:27:57 -0400

    initial iteration through all of the files

--- soen472/src/soen472/Main.java
+++ soen472/src/soen472/Main.java
@@ -1,11 +1,41 @@
 package soen472;
+import java.io.File;
+import java.io.IOException;
+import java.lang.String;
 
 public class Main {
+	
+	static String hamPath = System.getProperty("user.dir")+"\\emails\\20030228_easy_ham_2\\easy_ham_2";
+	static String spamPath = System.getProperty("user.dir")+"\\emails\\20030228_spam_2\\spam_2";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println(System.getProperty("user.dir"));
+		System.out.println(hamPath);
+		System.out.println(spamPath);
 
+		File f = new File(hamPath); // current directory
+
+	    File[] files = f.listFiles();
+	    int count = 0;
+	    for (File file : files) {
+	        if (file.isDirectory()) {
+	            System.out.print("directory:");
+	        } else {
+	            System.out.print("     file:");
+	        }
+	        try {
+				System.out.println(file.getCanonicalPath());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        count++;
+	        if (count >= 1000){
+	        	break;
+	        }
+	    }
+	    System.out.println(count);
 	}
 
 }
