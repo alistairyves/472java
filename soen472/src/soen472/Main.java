6b791d5be0d6f011f3dc4454fcf7dbfec956c1e1
Alistair Mersereau <alistairyves@gmail.com>
2014-10-26 11:14:03 -0400

    update the algorithm, both sets of files save all words into a hashmap, all that remains is processing these maps so that we can count the frequency of which words appear and calculate the probabilities.

--- soen472/src/soen472/Main.java
+++ soen472/src/soen472/Main.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 import java.lang.String;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
 	
 	static String hamPath = System.getProperty("user.dir")+"\\emails\\20030228_easy_ham_2\\easy_ham_2";
 	static String spamPath = System.getProperty("user.dir")+"\\emails\\20030228_spam_2\\spam_2";
 	
-	static HashMap<String, Integer> HamMap = new HashMap<String, Integer>();
-	HashMap<String, Integer> SpamMap = new HashMap<String, Integer>();
+	static TreeMap<String, Integer> HamMap = new TreeMap<String, Integer>();
+	static TreeMap<String, Integer> SpamMap = new TreeMap<String, Integer>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -20,28 +21,46 @@ public class Main {
 		System.out.println(hamPath);
 		System.out.println(spamPath);
 
-		File f = new File(hamPath); // current directory
+		File hamDir = new File(hamPath);
+		File spamDir = new File(spamPath);
 
-	    File[] files = f.listFiles();
-	    File tempf = null;
+	    File[] hamFiles = hamDir.listFiles();
+	    File[] spamFiles = spamDir.listFiles();
+	    
 	    int count = 0;
-	    for (File file : files) {
+	    for (File file : hamFiles) {
 	        if (file.isDirectory()) {
 	            System.out.print("directory:");
 	        } else {
 	            //System.out.print("file: \t");
 	        }
+	        processHamFile(file);
 	        //System.out.println(file.getAbsolutePath());
 	        count++;
 	        if (count >= 1000){
 	        	break;
 	        }
-	        tempf = file;
-	    }	    
+	        //tempf = file;
+	    }	 
+	    count = 0;
+	    for (File file : spamFiles) {
+	        if (file.isDirectory()) {
+	            System.out.print("directory:");
+	        } else {
+	            //System.out.print("file: \t");
+	        }
+	        processSpamFile(file);
+	        //System.out.println(file.getAbsolutePath());
+	        count++;
+	        if (count >= 1000){
+	        	break;
+	        }
+	        //tempf = file;
+	    }
 	    System.out.println(count);
-	    processHamFile(tempf);
-	    
+
 	    System.out.println(HamMap);
+	    System.out.println(SpamMap);
 	    
 	    
 	}
@@ -50,13 +69,32 @@ public class Main {
 			Scanner scanner = new Scanner(file);
 			while(scanner.hasNextLine()){
 				String line = scanner.nextLine();
-				System.out.println(line);
-				String words[] = line.split("\\W+");
+				String words[] = line.split("\\W*(\\W|_)\\W*");//splits on non alpha-numeric characters and ___
 				for(int i =0; i<words.length; i++){
-					if(words[i].length()<3)
+					if(wordCheck(words[i]))
 						continue;
 					addEntry(HamMap, words[i].toLowerCase());
-					System.out.println(words[i]);
+//					System.out.println(words[i]);
+				}
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	
+		
+	}
+	private static void processSpamFile(File file){
+		try {
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()){
+				String line = scanner.nextLine();
+				String words[] = line.split("\\W*(\\W|_)\\W*");//splits on non alpha-numeric characters and ___
+				for(int i =0; i<words.length; i++){
+					if(wordCheck(words[i]))
+						continue;
+					addEntry(SpamMap, words[i].toLowerCase());
+//					System.out.println(words[i]);
 				}
 			}
 			scanner.close();
@@ -67,7 +105,7 @@ public class Main {
 		
 	}
 	
-	private static void addEntry(HashMap map, String strEntry)
+	private static void addEntry(TreeMap<String, Integer> map, String strEntry)
 	{
 		if(map.containsKey(strEntry))
 		{
@@ -75,6 +113,21 @@ public class Main {
 		}
 		else
 			map.put(strEntry, 1);
+	}
+	static boolean wordCheck(String str){
+		//There are very few constraints at the moment, but more can be added
+		if(str.length()<3 || numCheck(str))
+			return true;
+		return false;
+	}
+	static boolean numCheck(String str){
+		for(int i = 0; i<str.length(); i++){
+			char c = str.charAt(i);
+			if(Character.isDigit(c))
+				return true; //returns true if there is a digit in the word
+		}
+		
+		return false;
 	}
 
 }
